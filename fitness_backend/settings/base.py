d0d12210a661dc08cad52090bce9c8862dea83fb
Jaolmos <jose.a.olmosmartinez@gmail.com>
2025-02-08 21:31:18 +0100

    config: actualizar requirements.txt del proyecto

--- fitness_backend/settings/base.py
+++ fitness_backend/settings/base.py
@@ -25,9 +25,11 @@ INSTALLED_APPS = [
     # Third party apps
     'rest_framework',
     'corsheaders',
+    'drf_yasg',
     # Local apps
     'apps.users',
     'apps.training',
+
 ]
 
 MIDDLEWARE = [
