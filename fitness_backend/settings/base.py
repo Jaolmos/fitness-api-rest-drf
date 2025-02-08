d8ef1d535f58d669cce1bbd1f84a7cb954121623
Jaolmos <jose.a.olmosmartinez@gmail.com>
2025-02-08 21:51:13 +0100

    feat: añadir y configurar JWT para autenticación

--- fitness_backend/settings/base.py
+++ fitness_backend/settings/base.py
@@ -32,6 +32,31 @@ INSTALLED_APPS = [
 
 ]
 
+# REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
+# JWT settings
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),  
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),     
+    'ROTATE_REFRESH_TOKENS': False,                  
+    'BLACKLIST_AFTER_ROTATION': False,            
+    
+    'AUTH_HEADER_TYPES': ('Bearer',),              
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',    
+    
+    'USER_ID_FIELD': 'id',                        
+    'USER_ID_CLAIM': 'user_id',                   
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -41,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    ]
 
 ROOT_URLCONF = 'fitness_backend.urls'
 
@@ -103,3 +128,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- fitness_backend/urls.py
+++ fitness_backend/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
-from drf_yasg import openapi # Módulo para definir la especificación OpenAPI
+from drf_yasg import openapi 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -35,6 +36,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # JWT URLs
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # Swagger URLs
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
